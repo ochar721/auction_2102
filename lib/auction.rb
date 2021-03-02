3448d03fe552ddb227a0f2294ab2fe76c09a7208
Olivia Charles <ocharles92@gmail.com>
2021-03-02 16:59:12 -0500

    Was working on closing bids.

--- lib/auction.rb
+++ lib/auction.rb
@@ -20,4 +20,21 @@ class Auction
       item.bids == {}
     end
   end
+
+  def potential_revenue
+    highest = []
+    @items.each do |item|
+      highest << item.current_high_bid
+    end
+    highest.compact.sum
+  end
+
+
+  def bidders
+    bidder_names = []
+      @items.each do |item|
+      bidder_names <<  item.bids
+      end
+      bidder_names[0].name
+    end
 end
