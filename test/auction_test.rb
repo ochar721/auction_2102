fc0fa17be28e7f592a6694c0ecbfb7b10bad16b0
Olivia Charles <ocharles92@gmail.com>
2021-03-02 15:17:27 -0500

    Created Auction test file

---
+++ test/auction_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/item'
+require './lib/atendee'
+require './lib/auction'
+
+class AuctionTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    auction = Auction.new
+    assert_instance_of Auction, auction
+  end
+
+  def test_it_starts_with_no_items
+    auction = Auction.new
+    assert_equal [], auction.items
+  end
+
+  def test_it_can_add_items
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+
+    assert_equal [item1, item2], auction.items
+    end
+
+  def test_it_has_item_names
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    attendee = Attendee.new(name: 'Megan', budget: '$50')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    assert_equal ["Chalkware Piggy Bank", "Bamboo Picture Frame"], auction.item_names
+  end
+end
--- test/item_test.rb
+++ test/item_test.rb
@@ -8,7 +8,7 @@ class ItemTest < Minitest::Test
     assert_instance_of Item, item1
     assert_equal "Chalkware Piggy Bank", item1.name
 
-   item2 = Item.new('Bamboo Picture Frame')
-   assert_equal "Bamboo Picture Frame", item2.name
+    item2 = Item.new('Bamboo Picture Frame')
+    assert_equal "Bamboo Picture Frame", item2.name
   end
 end
