4776dc03df466d0217c60f1bbfccb72e118639ae
Olivia Charles <ocharles92@gmail.com>
2021-03-02 16:58:39 -0500

    Finished writing tests for iteration 3

--- test/auction_test.rb
+++ test/auction_test.rb
@@ -130,5 +130,185 @@ class AuctionTest < Minitest::Test
   item4.add_bid(attendee3, 50)
 
   assert_equal [item2, item3, item5], auction.unpopular_items
+
+  item3.add_bid(attendee2, 15)
+  assert_equal [item2, item5], auction.unpopular_items
+  end
+
+  def test_it_has_potential_revenue
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    assert_equal 87, auction.potential_revenue
+  end
+
+  def test_it_has_bidders
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    assert_equal ["Megan", "Bob", "Mike"], auction.bidders
+    end
+
+  def test_it_has_bidder_info
+    skip
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    # expected = {attendee1  @budget=50, @name="Megan"=>
+    #       {
+    #         :budget => 50,
+    #         :items => [item1]
+    #       },
+    #     attendee2 @budget=75, @name="Bob" =>
+    #       {
+    #         :budget => 75,
+    #         :items => [item1, item3],
+    #       },
+    #     attendee3 @budget=100, @name="Mike" =>{
+    #         :budget => 100,
+    #         :items => [item4]}
+    #    }
+    assert_equal  "", auction.bidder_info
+  end
+
+  def test_item_closing_bids
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee2, 20)
+    item1.add_bid(attendee1, 22)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+
+    assert_equal 87, auction.potential_revenue
+  end
+
+  def test_it_has_bidders
+    skip
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    assert_equal ["Megan", "Bob", "Mike"], auction.bidders
+    end
+
+  def test_it_has_closing_bids
+    item1 = Item.new('Chalkware Piggy Bank')
+    item2 = Item.new('Bamboo Picture Frame')
+    item3 = Item.new('Homemade Chocolate Chip Cookies')
+    item4 = Item.new('2 Days Dogsitting')
+    item5 = Item.new('Forever Stamps')
+    attendee1 = Attendee.new(name: 'Megan', budget: '$50')
+    attendee2 = Attendee.new(name: 'Bob', budget: '$75')
+    attendee3 = Attendee.new(name: 'Mike', budget: '$100')
+    auction = Auction.new
+
+    auction.add_item(item1)
+    auction.add_item(item2)
+    auction.add_item(item3)
+    auction.add_item(item4)
+    auction.add_item(item5)
+
+    item1.add_bid(attendee1, 22)
+    item1.add_bid(attendee2, 20)
+    item4.add_bid(attendee3, 50)
+    item3.add_bid(attendee2, 15)
+    expected =   expected = {
+                  attendee1 => 22,
+                  attendee2=> 20
+                  }
+      assert_equal expected, item1.bids
+
+      item1.close_bidding
+
+      item1.add_bid(attendee3, 70)
+
+      expected =   expected = {
+                    attendee1 => 22,
+                    attendee2=> 20
+                    }
+    assert_equal expected, item1.bids
   end
 end
